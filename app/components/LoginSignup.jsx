7075f863ecdb5d4f3b442f7ba1e7d10753588904
L-Pircy <taveesha.agarwal@gmail.com>
2023-10-02 12:56:12 +0530

    login page disp

---
+++ app/components/LoginSignup.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const LoginSignup = () => {
+  return (
+    <div className="bg-[#21212198] backdrop-blur-md fixed inset-0 grid">
+      <div className="bg-[#eeeeee] rounded-xl place-self-center">xyz</div>
+    </div>
+  );
+};
+
+export default LoginSignup;
