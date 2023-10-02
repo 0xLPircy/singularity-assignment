4f80dc2afe578bd07e7bdf3f6c6a4978bcab8eb0
L-Pircy <taveesha.agarwal@gmail.com>
2023-10-02 14:43:54 +0530

    pop up functionality

--- app/components/LoginSignup.jsx
+++ app/components/LoginSignup.jsx
@@ -1,10 +1,29 @@
 import React from "react";
+import cross from "@/assets/crossIcon.png";
+import Image from "next/image";
 
-const LoginSignup = () => {
+const LoginSignup = ({ vis, onClosing }) => {
+  const checkArea = (e) => {
+    if (e.target.id === "blurArea") {
+      onClosing();
+    }
+  };
   return (
-    <div className="bg-[#21212198] backdrop-blur-md fixed inset-0 grid">
-      <div className="bg-[#eeeeee] rounded-xl place-self-center">xyz</div>
-    </div>
+    <>
+      {vis == "true" && (
+        <div
+          className="bg-[#21212198] backdrop-blur-md fixed inset-0 grid grid-flow-col"
+          onClick={checkArea}
+          id="blurArea"
+        >
+          <div className="bg-[#eeeeee] rounded-xl place-self-center">
+            <div>
+              <Image src={cross} height={30} width={30} onClick={onClosing} />
+            </div>
+          </div>
+        </div>
+      )}
+    </>
   );
 };
 
--- app/page.jsx
+++ app/page.jsx
@@ -1,21 +1,29 @@
+"use client";
 import Image from "next/image";
 import { LoginSignup } from "./components";
+import React, { useState } from "react";
 
 export default function Home() {
+  const [vis, SetVis] = useState("false");
+  const changeVis = () => {
+    SetVis("true");
+  };
+  console.log(vis);
   return (
-    <>
-      {/* buttons for different popups */}
+    <div>
       <div className="grid grid-flow-col gap-20 h-[100vh] w-[100vw] text-[#eeeeee] bg-[#2121216a]">
         {" "}
-        <button className="inset-0 bg-[#0d7dff] px-6 py-2 rounded-full  w-fit h-fit border-2 border-solid border-[#eeeeeee] self-center place-self-end hover:bg-[#3499ff]">
+        <button
+          onClick={changeVis}
+          className="inset-0 bg-[#0d7dff] px-6 py-2 rounded-full  w-fit h-fit border-2 border-solid border-[#eeeeeee] self-center place-self-end hover:bg-[#3499ff]"
+        >
           Login/ SignUp
         </button>{" "}
         <button className="inset-0 bg-[#0d7dff] px-6 py-2 rounded-full w-fit h-fit border-2 border-solid border-[#eeeeeee] self-center place-self-start hover:bg-[#3499ff]">
           Book a Free Session!
         </button>
       </div>
-      {/* popup */}
-      <LoginSignup />
-    </>
+      <LoginSignup vis={vis} onClosing={() => SetVis("false")} />
+    </div>
   );
 }
